5c592e288b04469c4b378e959fe981c7a26e9b29
RyanBegnaud <Ryanbegnaud13@gmail.com>
2021-01-22 12:00:54 -0800

    tweaked some css

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,17 @@ Rails.application.routes.draw do
    get '/signin', to: 'sessions#new'
    post '/signin', to: 'sessions#create'
    get '/signup', to: 'users#new'
+   get '/auth/facebook/callback' => 'sessions#create'
+   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+   get 'logout', to: 'sessions#destroy'
+
+   resources :goals
+   resources :comments
+
    resources :users do 
       resources :goals, only: [:show, :index]
    end
    resources :goals do 
       resources :comments, only: [:show, :new, :index, :create]
    end
-
-   resources :goals 
-   resources :comments
-   get '/auth/facebook/callback' => 'sessions#create'
-   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-   get 'logout', to: 'sessions#destroy'
 end
